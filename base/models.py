fd88a647934930ad3aabd6572c6b24363f4f6b67
Alan <alanthomasjohns99@gmail.com>
2022-12-03 19:47:25 +0530

    final stage

---
+++ base/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+import uuid
+# Create your models here.
+
+
+class Question(models.Model):
+
+    TYPES = (
+        ('backend', 'backend'),
+        ('frontend', 'frontend'),
+        ('fullstack', 'fullstack'),
+    )
+
+    answer = models.CharField(max_length=200, choices=TYPES)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4,  unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.answer
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    thumbnail = models.ImageField(null=True)
+    body = models.TextField()
+    slug = models.SlugField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4,  unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+
+
+class FeedBack(models.Model):
+    name = models.CharField(max_length=200)
+    feedback = models.TextField(null=True, blank=True)
+    # logo = models.ImageField(null=True, default="skill.png")
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4,  unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    body = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.body[0:50]
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4,  unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    subject = models.CharField(max_length=200, null=True)
+    body = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4,  unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
---
+++ base/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.homePage, name="home"),
+    path('project/<str:pk>/', views.projectPage, name="project"),
+
+    path('add-project/', views.addProject, name="add-project"),
+    path('edit-project/<str:pk>/', views.editProject, name="edit-project"),
+
+    path('inbox/', views.inboxPage, name="inbox"),
+    path('message/<str:pk>/', views.messagePage, name="message"),
+
+    path('give-me-feedback/', views.givefeedback, name="add-skill"),
+
+    path('donation/', views.donationPage, name="donation"),
+
+    path('chart/', views.chartPage, name="chart"),
+]
